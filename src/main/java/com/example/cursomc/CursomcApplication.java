aebd4f4795884c9fcb08ecd17d266001e56b90ce
Vinicius Thiago <vtls@cin.ufpe.br>
2020-05-15 21:42:55 -0300

    Cliente, tipoCliente, telefones e enderecos

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.domain.Cidade;
+import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.domain.Endereco;
 import com.example.cursomc.domain.Estado;
 import com.example.cursomc.domain.Produto;
+import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.repositories.CategoriaRepository;
 import com.example.cursomc.repositories.CidadeRepository;
+import com.example.cursomc.repositories.ClienteRepository;
+import com.example.cursomc.repositories.EnderecoRepository;
 import com.example.cursomc.repositories.EstadoRepository;
 import com.example.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EstadoRepository estado;
+	
+	@Autowired
+	private EnderecoRepository endereco;
+	
+	@Autowired
+	private ClienteRepository cliente;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -70,7 +81,20 @@ public class CursomcApplication implements CommandLineRunner {
 		 
 		 cidade.saveAll(Arrays.asList(c1,c2,c3));
 		 
+		 Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		 
+		 cli1.getTelefones().add("27363323");
+		 cli1.getTelefones().add("93838393");
+		 
+		 Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		 
+		 Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		 
+		 cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		 
+		 cliente.saveAll(Arrays.asList(cli1));
+
+		 endereco.saveAll(Arrays.asList(e1,e2));
 	}
 
 }
